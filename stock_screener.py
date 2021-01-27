c192363b009e92796b2bbfb9daab0b27b6fef5e3
RekaCodes <degree325@gmail.com>
2021-01-27 15:23:47 -0500

    added chart type radio selector

--- stock_screener.py
+++ stock_screener.py
@@ -1,12 +1,11 @@
 import streamlit as st
-
+st.set_page_config(layout='centered')
 import pandas as pd
 
 import matplotlib.pyplot as plt
 import mplfinance as mpl
 
 import yfinance as yf
-st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
 # build app #
@@ -16,9 +15,31 @@ def main():
 
     st.title("Stock Screener")
 
+    @st.cache()
+    def load_data():
+        data=yf.download(['SPY', 'NDAQ', 'DJIA'], period='3mo', interval='1d', group_by='ticker')
 
-    filter_stock = st.text_input(
-        label='Insert ticker symbol.')
+        return data
+    
+    data=load_data()
+    
+    # col1, col2, col3 = st.beta_columns(3)
+    # with col1:
+    #     # spy_data=yf.download('SPY', period='6mo', interval='1d', group_by='ticker', auto_adjust=True)
+    #     sp_plot= mpl.plot(data['SPY'], type='line', style='mike', volume=True,  title='S&P500')
+    #     st.pyplot(fig=sp_plot)
+    # with col2:
+    #     # ndaq_data=yf.download('NDAQ', period='6mo', interval='1d', group_by='ticker', auto_adjust=True)
+    #     ndaq_plot= mpl.plot(data['NDAQ'], type='line', style='mike', volume=True,  title='NASDAQ')
+    #     st.pyplot(fig=ndaq_plot)
+    # with col3:
+    #     # dow_data=yf.download('DJIA', period='6mo', interval='1d', group_by='ticker', auto_adjust=True)
+    #     dow_plot= mpl.plot(data['DJIA'], type='line', style='mike', volume=True,  title='DOW')
+    #     st.pyplot(fig=dow_plot)
+
+
+    st.header('Stock Search')
+    
     
     filter_period = '6mo'
     filter_interval = '1d'
@@ -26,20 +47,29 @@ def main():
     filter_volume = False
 
 
-    col1, col2 = st.beta_columns(2)
+    col1, col2, col3, col4 = st.beta_columns(4)
     with col1:
+        filter_stock = st.text_input(
+        label='Insert ticker symbol.')
+        
+    with col2:
+        chart_type = st.radio('Chart Type:',['line', 'candle', 'pnf'])
+        
+    with col3:
         filter_period = st.selectbox(
             'Time period:',
             key='6mo',
             options= ['1mo', '3mo', '6mo', '1y', '2y']
         )
-    with col2:
+    with col4:
         filter_interval = st.select_slider(
             'Interval:',
             key='1d',
             options=['1h', '1d', '1wk']
         )
-    
+
+
+
     # col3, col4 = st.beta_columns(2)
     # with col3:
         # check_avg = st.checkbox('Moving Average:')
@@ -58,9 +88,9 @@ def main():
 
         st.markdown("#")
 
-        fig1= mpl.plot(data=data[filter_stock.upper()], type='candle', style='charles', mav=(5,20,50), volume=True,  title=filter_stock.upper())
+        stock_plot= mpl.plot(data=data[filter_stock.upper()], type=chart_type, style='charles', mav=(5,20,50), volume=True,  title=filter_stock.upper())
         
-        st.pyplot(fig=fig1)
+        st.pyplot(fig=stock_plot)
 
         with st.beta_expander(label='Click for data'):
             st.write(data[filter_stock.upper()])
