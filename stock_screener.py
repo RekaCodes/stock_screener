e3f9fa01241cfac5c29c8d664ab9ef12b7528387
RekaCodes <degree325@gmail.com>
2021-01-28 22:27:40 -0500

    convert to webpage with sidebar

--- stock_screener.py
+++ stock_screener.py
@@ -4,61 +4,79 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import mplfinance as mpl
 import yfinance as yf
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
 
 
 # build app #
 def main():
+    ### sidebar ###
 
+    st.sidebar.header('Chart Setup.')
+    filter_period = st.sidebar.select_slider(
+            'Time period:',
+            value='6mo',
+            options= ['1mo', '3mo', '6mo', '1y', '2y']
+        )
+
+    filter_interval = st.sidebar.selectbox(
+            'Interval:',
+            index=1,
+            options=['1h', '1d', '1wk']
+        )
+
+    chart_type = st.sidebar.radio('Chart Type:',['Candle', 'OHLC', 'Line', 'PNF'])
+
+
+
+    ### webpage ###
     st.title("Stock Screener")
 
     filter_period = '6mo'
     filter_interval = '1d'
+    chart_type = 'candle'
 
-    col1, col2, col3 = st.beta_columns([3,2,5])
+    col1, col2 = st.beta_columns([1,3])
     with col1:
-        st.markdown("####")
-        st.subheader("Insert ticker symbol:")
-    with col2:
         filter_stock = st.text_input(label="", value='AAPL')
-    with col3:
+    with col2:
         st.write("")
-    
-
-    
-    col3, col4, col5 = st.beta_columns([2,1,1])
-    with col3:
-        st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-        chart_type = st.radio('Chart Type:',['Candle', 'OHLC', 'Line', 'PNF'])
-
-    with col4:
-        st.markdown("####")
-        filter_period = st.selectbox(
-            'Time period:',
-            key='6mo',
-            options= ['1mo', '3mo', '6mo', '1y', '2y']
-        )
-
-    with col5:
-        st.markdown("####")
-        filter_interval = st.select_slider(
-            'Interval:',
-            key='1d',
-            options=['1h', '1d', '1wk']
-        )
-
     if st.button('Get stock data'):
-        data=yf.download([filter_stock.upper(), 'SPY'], period=filter_period, interval=filter_interval, group_by='ticker', auto_adjust=True)
+        chart_data=yf.download([filter_stock.upper(), 'SPY'], period=filter_period, interval=filter_interval, group_by='ticker', auto_adjust=True)
+        stock_data = yf.Ticker(filter_stock.upper())
 
-        st.markdown("#")
+        st.header(stock_data.info['shortName'])
 
-        stock_plot= mpl.plot(data=data[filter_stock.upper()], type=chart_type.lower(), style='yahoo', mav=(5,10,25), volume=True,  title=filter_stock.upper())
-        
-        st.pyplot(fig=stock_plot)
+        col7, col8, col9 = st.beta_columns(3)
+        with col7:
+            st.title('${:0,.2f}'.format(stock_data.info['ask']))
+        with col9:
+            st.write('52W High| ${:0,.2f}'.format(stock_data.info['fiftyTwoWeekHigh']))
+            st.write('52W Low| ${:0,.2f}'.format(stock_data.info['fiftyTwoWeekLow']))
+
+        with col8:
+            close_prior = stock_data.info['previousClose']
+            ask_current = stock_data.info['ask']
+            chg_frm_close = ask_current - close_prior
+            pct_chg_frm_close = chg_frm_close/close_prior
+            chg_frm_close = "${:0,.2f}".format(chg_frm_close).replace('$-','-$')
+            pct_chg_frm_close = "{:.1%}".format(pct_chg_frm_close).replace('$-','-$')
+            # st.write(stats_data.iloc[1,[filter_stock.upper()]['Close']])
+            # st.write('Change from Close ')
+        # st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+            st.header(f"{chg_frm_close} {pct_chg_frm_close}")
+            st.write("Change from close")
+            # st.header('{:.1%}'.format(chg_frm_close))
+
+
+        stock_fig = mpl.plot(data=chart_data[filter_stock.upper()], type=chart_type.lower(), style='yahoo', mav=(5,10,25), volume=True, title=filter_stock.upper())
+        st.pyplot(stock_fig)
+
+        st.write(stock_data.info)
 
         with st.beta_expander(label='Click for data'):
-            st.write(data[filter_stock.upper()])
+            st.write(chart_data[filter_stock.upper()])
 
 
 if __name__=='__main__':
